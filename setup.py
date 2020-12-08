df0b40d83869046b469daf3e16bcbfbaae790447
skywalker0803r <41248543+skywalker0803r@users.noreply.github.com>
2020-12-08 20:43:03 +0800

    Update setup.py
    123

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "autorch",
-    version = "0.2.0",
+    version = "0.2.1",
     description = ("auto pytorch"),
     packages = find_packages(),
     url="https://github.com/skywalker0803r/autorch",
