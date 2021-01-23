5c335469f0c104e04fb7547b1a50e1d5529a6297
skywalker0803r <41248543+skywalker0803r@users.noreply.github.com>
2021-01-23 20:45:19 +0800

    Update __init__.py

--- autorch/__init__.py
+++ autorch/__init__.py
@@ -1,4 +1,7 @@
 import os
-os.system("pip install git+https://github.com/jonbarron/robust_loss_pytorch")
+try:
+  import robust_loss_pytorch
+except:
+  os.system("pip install git+https://github.com/jonbarron/robust_loss_pytorch")
 from . import utils,function,transferlearning
 
